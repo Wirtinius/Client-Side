f63a6d99db4ecb532533792627e3ab00c9f36c6f
Wirtinius <alenanel2004@gmail.com>
2024-11-01 08:45:44 +0100

    feat: added profile

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 let currentIndex = 0;
-const slides = document.querySelectorAll('.slide');
+let currentPresentation = 1;
+const lastViewedSlide = { 1: 0, 2: 0 };
+const presentations = {
+  1: document.querySelectorAll('.presentation-1'),
+  2: document.querySelectorAll('.presentation-2'),
+};
 const slideCountDisplay = document.getElementById('slide-count');
-updateSlideCount();
 
 document.getElementById('next-slide').addEventListener('click', () => {
-  if (currentIndex < slides.length - 1) {
+  if (currentIndex < presentations[currentPresentation].length - 1) {
     changeSlide(currentIndex + 1);
   }
 });
@@ -15,17 +19,52 @@ document.getElementById('previous-slide').addEventListener('click', () => {
   }
 });
 
+document.getElementById('next-presentation').addEventListener('click', () => {
+  changePresentation(2);
+});
+
+document.getElementById('previous-presentation').addEventListener('click', () => {
+  changePresentation(1);
+});
+
 function changeSlide(index) {
+  const slides = presentations[currentPresentation];
   slides[currentIndex].classList.remove('active', 'prev');
   slides[currentIndex].classList.add(index > currentIndex ? 'prev' : 'next');
   currentIndex = index;
   slides[currentIndex].classList.remove('next', 'prev');
   slides[currentIndex].classList.add('active');
   updateSlideCount();
+  lastViewedSlide[currentPresentation] = currentIndex;
+}
+
+function changePresentation(presentation) {
+
+  lastViewedSlide[currentPresentation] = currentIndex;
+
+  currentPresentation = presentation;
+  currentIndex = lastViewedSlide[currentPresentation];
+
+  presentations[1].forEach((slide) => slide.classList.add('hidden'));
+  presentations[2].forEach((slide) => slide.classList.add('hidden'));
+
+  presentations[currentPresentation].forEach((slide) => slide.classList.remove('hidden'));
+  changeSlide(currentIndex);
+
+  const header = document.querySelector('header');
+  const buttons = document.querySelectorAll('button');
+
+  if (currentPresentation === 1) {
+    header.style.backgroundColor = '#4c9f70';
+    buttons.forEach(button => button.style.backgroundColor = '#4c9f70');
+  } else {
+    header.style.backgroundColor = '#e65c4c';
+    buttons.forEach(button => button.style.backgroundColor = '#e65c4c');
+  }
 }
 
 function updateSlideCount() {
-  slideCountDisplay.innerText = `${currentIndex + 1} / ${slides.length}`;
+  slideCountDisplay.innerText = `${currentIndex + 1} / ${presentations[currentPresentation].length}`;
 }
 
 changeSlide(0);
